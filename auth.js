950850c5190254118f2f304fe1e5c4194837523f
root <vijaysemalty@gmail.com>
2022-06-03 07:03:55 +0000

    Fixed it text for welcome

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate (login,password) {
 	  if (login == 'login' && password === 'password') {
-		  return 'Your are logged in!';
+		  return 'Welcome';
 	  }	  else {
 		  return 'Login is not correct';
 	  }
