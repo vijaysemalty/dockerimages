cc714bd547c089ac89213ca6ae766d704b7c84c0
root <vijaysemalty@gmail.com>
2022-06-03 11:11:07 +0000

    Simplified Authentication

--- auth.js
+++ auth.js
@@ -1,7 +1,3 @@
 function authenticate (login,password) {
-	  if (login == 'login' && password === 'password') {
-		  return 'Welcome2';
-	  }	  else {
-		  return 'Login is not correct';
-	  }
+	  return 'Welcome'
 }
