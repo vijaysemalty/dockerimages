381c52e3cb3f78c74ebbccb5edebb3638499f554
root <vijaysemalty@gmail.com>
2022-06-03 06:52:56 +0000

    auth file modification

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate (login,password) {
 	  if (login == 'login' && password === 'password') {
-		  return 'Your are logged in';
+		  return 'Your are logged in!';
 	  }	  else {
 		  return 'Login is not correct';
 	  }
