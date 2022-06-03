e2cc00538a26d4c883e309bb77b693550c286662
root <vijaysemalty@gmail.com>
2022-06-03 06:41:03 +0000

    Added authentication

---
+++ auth.js
@@ -0,0 +1,7 @@
+function authenticate (login,password) {
+	  if (login == 'login' && password === 'password') {
+		  return 'Your are logged in';
+	  }	  else {
+		  return 'Login is not correct';
+	  }
+}
