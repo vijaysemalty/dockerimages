d22d0ccaac14157cbea850a74b95db3f90120fe6
root <vijaysemalty@gmail.com>
2022-06-05 11:53:03 +0000

    changingAuthfile_message

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate (login,password{
 	  if (login == 'login' && password === 'password') {
-		  return 'Hi Everybody';
+		  return 'Merging to master';
 	  }	  else {
 		  return 'Login is not correct';
 	  }
